5eb60c51063d1a758cc15ad2fff800d7683724f8
kimkyeonghyun <khyun8072@gmail.com>
2024-12-02 15:36:37 +0900

    modifying scripts

--- Classification/run_classification.sh
+++ Classification/run_classification.sh
@@ -1,26 +1,19 @@
-clear
-SEED_LIST=(42 1004 2023 2024 2025)
-
-
 TASK_LIST=(classification)
 LLM=llama3.1
 CLASSIFICATION_TASK_LIST=(imdb tweet_sentiment_binary cr)
 TEST_DATASET_LIST=(imdb tweet_sentiment_binary cr)
- 
+
 # 실험 1-a
 for TASK in ${TASK_LIST[@]}; do
     if [ $TASK == 'classification' ]; then
         for CLASSIFICATION_TASK in ${CLASSIFICATION_TASK_LIST[@]}; do
             for MODEL in ${MODEL_LIST[@]}; do
-                for SEED in ${SEED_LIST[@]}; do
-                    python main.py --task $TASK --job=preprocessing --task_dataset=$CLASSIFICATION_TASK --model_type=$MODEL --seed=$SEED 
-                    python main.py --task $TASK --job=training --task_dataset=$CLASSIFICATION_TASK --test_dataset=$CLASSIFICATION_TASK --model_type=$MODEL --seed=$SEED --method=base
-                    python main.py --task $TASK --job=testing --task_dataset=$CLASSIFICATION_TASK --test_dataset=$CLASSIFICATION_TASK --model_type=$MODEL --seed=$SEED --method=base
-                    python main.py --task $TASK --job=training --task_dataset=$CLASSIFICATION_TASK --test_dataset=$CLASSIFICATION_TASK --model_type=$MODEL --seed=$SEED --method=base_llm --llm=$LLM
-                    python main.py --task $TASK --job=testing --task_dataset=$CLASSIFICATION_TASK --test_dataset=$CLASSIFICATION_TASK --model_type=$MODEL --seed=$SEED --method=base_llm --llm=$LLM
-                done
+                python main.py --task $TASK --job=preprocessing --task_dataset=$CLASSIFICATION_TASK --model_type=$MODEL
+                python main.py --task $TASK --job=training --task_dataset=$CLASSIFICATION_TASK --test_dataset=$CLASSIFICATION_TASK --model_type=$MODEL --method=base
+                python main.py --task $TASK --job=testing --task_dataset=$CLASSIFICATION_TASK --test_dataset=$CLASSIFICATION_TASK --model_type=$MODEL --method=base
+                python main.py --task $TASK --job=training --task_dataset=$CLASSIFICATION_TASK --test_dataset=$CLASSIFICATION_TASK --model_type=$MODEL --method=base_llm --llm=$LLM
+                python main.py --task $TASK --job=testing --task_dataset=$CLASSIFICATION_TASK --test_dataset=$CLASSIFICATION_TASK --model_type=$MODEL --method=base_llm --llm=$LLM
             done
         done
     fi
 done
-
