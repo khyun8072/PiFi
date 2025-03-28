8caa79a972e6e8f3ba6f62bc2f50a3d966f4be22
kimkyeonghyun <108453684+kimkyeonghyun@users.noreply.github.com>
2025-03-28 16:20:33 +0900

    Delete slm_smol.sh

--- slm_smol.sh
+++
@@ -1,7 +0,0 @@
-datasets=(SST2 TweetSentimentBinary)
-
-
-for dataset in "${datasets[@]}"
-do
-    python SLM_smollm.py --dataset "$dataset" --model_name HuggingFaceTB/SmolLM2-360M --padding_side right
-done
