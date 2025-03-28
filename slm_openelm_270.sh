fb00e296b3885d8c5e56fb36ab6dba6953e59aed
kimkyeonghyun <108453684+kimkyeonghyun@users.noreply.github.com>
2025-03-28 16:20:15 +0900

    Delete slm_openelm_270.sh

--- slm_openelm_270.sh
+++
@@ -1,6 +0,0 @@
-datasets=(SST2 TweetSentimentBinary)
-
-for dataset in "${datasets[@]}"
-do
-    python SLM_openelm.py --dataset "$dataset" --model_name apple/OpenELM-270M --padding_side right
-done
